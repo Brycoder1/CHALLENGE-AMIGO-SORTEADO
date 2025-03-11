668002a421c30bb58fa71604febf0535aa76bf7e
Brycoder1 <162774580+Brycoder1@users.noreply.github.com>
2025-03-11 14:11:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ function agregarAmigo () {
 
 
 
-
-
 function actualizarlista () {
     let listaAmigos = document.getElementById('listaAmigos');   
     listaAmigos.innerHTML = "";
@@ -31,12 +29,7 @@ function actualizarlista () {
         let item = document.createElement("li");
         item.textContent = amigoS[i];
         listaAmigos.appendChild(item);
-        console.log(amigoS)
-
-    }
-
 };
-
 
 
 
@@ -53,11 +46,58 @@ function sortearAmigo () {
 };
 
 
+function enlazarAmigo() {
+
+    amigoS = amigoS.sort(() => Math.random() - 0.5);
+
+    let result = []; 
+    for (let i = 0; i < amigoS.length; i+= 2) {
+    if (i + 1 < amigoS.length) {
+
+        result.push(`${amigoS[i]} & ${amigoS[i + 1]}`);
+        } else {
+         result.push(`${amigoS[i]}  (Sol(@)`);
+        }
+     
+
+    let listaParejas =document.getElementById("parejas");
+    listaParejas.innerHTML = "";
+    result.forEach(par => {
+
+    let item = document.createElement("li");
+    item.textContent = par;
+    listaParejas.appendChild(item);})
+    limpiarLista
+}};
+
+    
 
 function limpiarLista () {
     amigoS = []
     document.getElementById('listaAmigos').innerHTML = '';
 }
+
+
+function reiniciarJuego () {
+
+    // Vaciar la lista de amigos
+        amigoS = [];
+        document.getElementById('listaAmigos').innerHTML = '';
+    
+        // Vaciar la lista de parejas
+        document.getElementById('parejas').innerHTML = '';
+    
+        // Vaciar el resultado del sorteo
+        document.getElementById('resultado').innerHTML = '';
+    
+        // Limpiar el input de nombres
+        document.getElementById('amigo').value = '';
+    
+        alert("El juego se ha reiniciado.");
+    }
+    
+    
+
 
 
 
